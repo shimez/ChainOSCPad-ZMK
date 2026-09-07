e371b7d2e7a653b44d4094a4a65b9710035d0ec3
shimez <ctake.shimez@gmail.com>
2026-09-07 19:43:01 +0900

    LED Test

--- src/led_init.c
+++ src/led_init.c
@@ -1,6 +1,7 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/init.h>
+#include <errno.h>
 
 #define LED_NODE DT_ALIAS(chainoscpad_led)
 
@@ -13,7 +14,12 @@ static int chainoscpad_led_init(void)
         return -ENODEV;
     }
 
-    return gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    int ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
+    if (ret < 0) {
+        return ret;
+    }
+
+    return gpio_pin_set_dt(&led, 1);
 }
 
 SYS_INIT(chainoscpad_led_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
